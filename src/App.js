bae546e68f985dbe80a7ab2a0f91c06d05a50645
Ashir N Basheer <ashirnbasheer@gmail.com>
2022-02-19 19:28:38 +0530

    styling completed for pc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './app.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-body'>
+     <h1> Currency Converter </h1>
+     <div className='input'>
+
+        <div className='first-row'>
+        <input  type="number"/>
+         <select>
+             <option value="hi" >hi</option>
+         </select>
+         <p ><i class="fa-solid fa-arrow-right-long"></i></p>
+         <select>
+             <option value="hi" >hi</option>
+         </select>
+        </div>
+
+
+
+        <div className='second-row'>
+        <button>Convert</button>
+         <div className='show'></div>
+         </div>
+
+
+
+
+        </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
